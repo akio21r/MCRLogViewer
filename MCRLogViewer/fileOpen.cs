4f0f8f18ecfd1054fabc6184825b97902927cf71
akio21r <akio21r@gmail.com>
2019-07-14 17:02:16 +0900

    LOG_Version=11（Cameraログ）追加、Ver=09も表示のみ変更

---
+++ MCRLogViewer/fileOpen.cs
@@ -0,0 +1,27 @@
+﻿/*
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+using MCRLogViewer.frmMain;
+
+namespace MCRLogViewer
+{
+	public partial class frmMain : Form{
+		public void FileOpen_Log009(){
+            int WorkAddress, BuffAddress;
+            int n=0, i;
+
+
+
+	
+	}
+}
+
+ */
